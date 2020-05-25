e59e1605ba642922513d664e702a0ea707052935
treasurebear94 <qvo7895@gmail.com>
2020-05-25 23:39:53 +0900

    const, var

--- main.go
+++ main.go
@@ -9,4 +9,11 @@ import (
 func main() {
 	fmt.Println("Hello world!")
 	something.SayHello()
+
+	const name string = "bear"
+	var name2 string = "bear"
+	name3 := "bear"
+	fmt.Println(name)
+	fmt.Println(name2)
+	fmt.Println(name3)
 }
