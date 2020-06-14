906b5b20a01ae581fd217e086ded0368fc2fd5b8
treasureBear94 <qvo7896@gmail.com>
2020-06-14 23:03:46 +0900

    1 account

--- main.go
+++ main.go
@@ -1,17 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-type person struct {
-	name    string
-	age     int
-	favFood []string
-}
+	"github.com/treasureBear94/learngo/accounts"
+)
 
 func main() {
-	favFood := []string{"kimchi", "apple"}
-	trea := person{"trea", 18, favFood}
-	trea2 := person{name: "trea", age: 18, favFood: favFood}
-	fmt.Println(trea.name)
-	fmt.Println(trea2.name)
+	account := accounts.NewAccount("treasure")
+	fmt.Println(account)
 }
