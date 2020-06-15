799a7d29d16249845c8c4e2f09b549fdb07947fa
treasureBear94 <qvo7896@gmail.com>
2020-06-15 22:46:33 +0900

    withdraw

--- main.go
+++ main.go
@@ -10,4 +10,10 @@ func main() {
 	account := accounts.NewAccount("treasure")
 	account.Deposit(10)
 	fmt.Println(account.Balance())
+	err := account.Withdraw(20)
+	if err != nil {
+		// log.Fatalln(err)
+		fmt.Println(err)
+	}
+	fmt.Println(account.Balance())
 }
