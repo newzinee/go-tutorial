fc713d239d5773a9a0d5e83c42b25909b8a6f752
treasureBear94 <qvo7896@gmail.com>
2020-06-17 23:30:05 +0900

    dict - update

--- main.go
+++ main.go
@@ -3,40 +3,17 @@ package main
 import (
 	"fmt"
 
-	"github.com/treasureBear94/learngo/accounts"
 	"github.com/treasureBear94/learngo/mydict"
 )
 
 func main() {
-	account := accounts.NewAccount("treasure")
-	account.Deposit(10)
-	fmt.Println(account.Balance())
-	err := account.Withdraw(20)
-	if err != nil {
-		// log.Fatalln(err)
-		fmt.Println(err)
-	}
-	fmt.Println(account.Balance(), account.Owner())
-	fmt.Println(account)
-
-	dictionary := mydict.Dictionary{"first": "First word"}
-	definition, err := dictionary.Search("first")
+	dictionary := mydict.Dictionary{}
+	baseWord := "hello"
+	dictionary.Add(baseWord, "First")
+	err := dictionary.Update(baseWord, "Second")
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println(definition)
 	}
-
-	word := "hello"
-	defi := "Greeting"
-	err2 := dictionary.Add(word, defi)
-	if err2 != nil {
-		fmt.Println(err2)
-	}
-	hello, _ := dictionary.Search(word)
-	fmt.Println(hello)
-	err3 := dictionary.Add(word, defi)
-	if err3 != nil {
-		fmt.Println(err3)
-	}
+	word, _ := dictionary.Search(baseWord)
+	fmt.Println(word)
 }
--- mydict/mydict.go
+++ mydict/mydict.go
@@ -5,8 +5,11 @@ import "errors"
 // Dictionary type
 type Dictionary map[string]string
 
-var errNotFound = errors.New("Not Found")
-var errWordExists = errors.New("That word already exists")
+var (
+	errNotFound   = errors.New("Not Found")
+	errWordExists = errors.New("That word already exists")
+	errCantUpdate = errors.New("Cant update non-existing word")
+)
 
 // Search for a word
 func (d Dictionary) Search(word string) (string, error) {
@@ -26,6 +29,18 @@ func (d Dictionary) Add(word, def string) error {
 		d[word] = def
 	case nil:
 		return errWordExists
+	}
+	return nil
+}
+
+// Update a word
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = definition
+	case errNotFound:
+		return errCantUpdate
 	}
 	return nil
 }
