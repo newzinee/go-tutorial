4010ff2061126b73c6f5bb2505635d44135d1744
treasureBear94 <qvo7896@gmail.com>
2020-06-17 23:32:37 +0900

    dict - Delete

--- main.go
+++ main.go
@@ -10,10 +10,11 @@ func main() {
 	dictionary := mydict.Dictionary{}
 	baseWord := "hello"
 	dictionary.Add(baseWord, "First")
-	err := dictionary.Update(baseWord, "Second")
+	dictionary.Delete(baseWord)
+	word, err := dictionary.Search(baseWord)
 	if err != nil {
 		fmt.Println(err)
+	} else {
+		fmt.Println(word)
 	}
-	word, _ := dictionary.Search(baseWord)
-	fmt.Println(word)
 }
--- mydict/mydict.go
+++ mydict/mydict.go
@@ -44,3 +44,8 @@ func (d Dictionary) Update(word, definition string) error {
 	}
 	return nil
 }
+
+// Delete a word
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
