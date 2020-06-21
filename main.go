3f131d80ac17d58dd1c88e9b4e3253bd28d731c4
treasureBear94 <qvo7896@gmail.com>
2020-06-21 22:33:59 +0900

    goroutines

--- main.go
+++ main.go
@@ -4,29 +4,21 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
-	var results = make(map[string]string)
+	// go sexyCount("trea")
+	// sexyCount("bear")
+	go sexyCount("trea")
+	go sexyCount("bear")
+	time.Sleep(time.Second * 5)
+}
 
-	urls := []string{
-		"https://www.airbnb.com",
-		"https://www.google.com",
-		"https://www.amazon.com",
-		"https://www.facebook.com",
-		"https://www.instagram.com",
-		"https://academy.nomadcoders.co",
-	}
-	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
-	}
-	for url, result := range results {
-		fmt.Println(url, result)
+func sexyCount(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(person, "is sexy", i)
+		time.Sleep(time.Second)
 	}
 }
 
