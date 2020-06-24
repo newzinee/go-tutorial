8329f6b400744a08de28ca83b815bf3b5e648998
treasureBear94 <qvo7896@gmail.com>
2020-06-24 21:27:11 +0900

    channel

--- main.go
+++ main.go
@@ -1,35 +1,23 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"net/http"
 	"time"
 )
 
 func main() {
-	// go sexyCount("trea")
-	// sexyCount("bear")
-	go sexyCount("trea")
-	go sexyCount("bear")
+	c := make(chan bool)
+	people := [2]string{"trea", "bear"}
+	for _, person := range people {
+		go isSexy(person, c)
+	}
+	result := <-c
+	fmt.Println(result)
+	fmt.Println(<-c)
+}
+
+func isSexy(person string, c chan bool) {
 	time.Sleep(time.Second * 5)
-}
-
-func sexyCount(person string) {
-	for i := 0; i < 10; i++ {
-		fmt.Println(person, "is sexy", i)
-		time.Sleep(time.Second)
-	}
-}
-
-var errRequestFaild = errors.New("Request failed")
-
-func hitURL(url string) error {
-	fmt.Println("Checking: ", url)
-	resp, err := http.Get(url)
-	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println(err, resp.StatusCode)
-		return errRequestFaild
-	}
-	return nil
+	fmt.Println(person)
+	c <- true
 }
